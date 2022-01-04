3b6f053d076076f3c3d40d4190542f9799a00423
Prajesh Jain <95735147+prajeshjain10@users.noreply.github.com>
2022-01-04 13:48:15 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const hostname="127.0.0.1";
-const port=80
-const fs=require("fs");
-const http=require('http');
-http.createServer(function (req, res){
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(80);
